6f1822c77f0279e7ce72a023894dce4bc86f9ec4
Wendell Rodrigues Amaral <wendellra@ciandt.com>
2022-04-27 14:32:32 -0300

    inicio de refatoração do modal

--- src/app/shared/components/modals/modal-card/modal-card.component.ts
+++ src/app/shared/components/modals/modal-card/modal-card.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit,TemplateRef } from '@angular/core';
+import { Component, Input, OnInit,TemplateRef } from '@angular/core';
 import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
+import { CovidCards } from 'src/app/models/covid-cards.model';
 @Component({
   selector: 'modal-card',
   templateUrl: './modal-card.component.html',
@@ -9,12 +10,17 @@ export class ModalCardComponent implements OnInit {
 
   modalRef?: BsModalRef;
   constructor(private modalService: BsModalService) {}
+  @Input() localizacaoModal!:CovidCards;
+  @Input() format!: Function;
+
+
+  ngOnInit(): void {
+    console.log(this.localizacaoModal)
+  }
  
   openModal(template: TemplateRef<any>) {
     this.modalRef = this.modalService.show(template);
   }
 
-  ngOnInit(): void {
-  }
 
 }
